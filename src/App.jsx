fbaf5513fd0fa6f0e3ddbe0d58e4ee0218b88207
yasubdemissie <yasdam777@gmail.com>
2024-10-26 17:34:40 +0300

    somethung

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
+        
         <Route path="/app" element={<AppLayout />} />
       </Routes>
     </BrowserRouter>
