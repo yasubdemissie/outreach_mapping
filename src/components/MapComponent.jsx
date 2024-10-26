f356f77ff09e7dee3a4d651ad26b11571140bbf5
misudesu <misaeldessalegn@gmail.com>
2024-10-26 20:22:16 +0300

    done

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,5 +1,6 @@
 import  { useCallback, useState } from 'react';
 import { GoogleMap, InfoWindowF, MarkerF, useJsApiLoader } from '@react-google-maps/api';
+import RecipeReviewCard from './RecipeReviewCard';
 
 const containerStyle = {
   width: '70vw',
@@ -10,13 +11,59 @@ const center = {
     lat: -3.745,
     lng: -38.523,
   };
-
+  const apifeatch=[
+    {
+      "location": {
+        "type": "Point",
+        "coordinates": [
+           -3.745,
+            -38.523
+        ]
+      },
+      "_id": "671c9bd152ade19a5a50ed70",
+      "name": "someone",
+      "evangelismMethod": "oneToOneOutreach",
+      "numOfPeopleReached": 20,
+      "numOfPeopleSaved": 10,
+      "numOfPeopleRepent": 2,
+      "area": 20,
+      "date": "Sep 20, 2024",
+      "description": "This is the description",
+      "image": 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
+      "createdAt": "2024-10-26T07:35:45.071Z",
+      "updatedAt": "2024-10-26T07:35:45.071Z",
+      "__v": 0
+    },
+    {
+        "location": {
+          "type": "Point",
+          "coordinates": [
+            9.0058687,
+            38.8072656
+          ]
+        },
+        "_id": "671c9bd152ade19a5a50ed70",
+        "name": "someone",
+        "evangelismMethod": "oneToOneOutreach",
+        "numOfPeopleReached": 20,
+        "numOfPeopleSaved": 10,
+        "numOfPeopleRepent": 2,
+        "area": 20,
+        "date": "Sep 20, 2024",
+        "description": "This is the description",
+        "image": null,
+        "createdAt": "2024-10-26T07:35:45.071Z",
+        "updatedAt": "2024-10-26T07:35:45.071Z",
+        "__v": 0
+      }
+  ]
 function MapComponent() {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: 'AIzaSyCtIGX5DjROORcpJAo8fNh2TD7S67FcVvM', // Replace with your API key
   });
-  const [markers, setMarkers] = useState([center,]); // Store all markers
+  const [markers, setMarkers] = useState([center,]);
+   // Store all markers
   const [selectedPosition, setSelectedPosition] = useState(null);
 
   const onLoad = useCallback((map) => {
@@ -45,6 +92,7 @@ function MapComponent() {
     setInfoWindowVisible(false);
     setSelectedPosition(null);
   };
+  console.log(selectedPosition)
   return isLoaded ? (
     <GoogleMap
       id="google-map"
@@ -71,17 +119,25 @@ function MapComponent() {
         onClick={() => handleMarkerClick(marker)}
       />
     ))}
+    {/* Render center marker */}
+    {apifeatch.map((marker, index) => (
+    <MarkerF
+      key={index}
+      position={{ lat: marker.location.coordinates[0], lng: marker.location.coordinates[1] }}
+      onClick={() => handleMarkerClick(marker)}
+    />
+   ) )}
 
     {infoWindowVisible && selectedPosition && (
+      
       <InfoWindowF
-        position={selectedPosition}
+        position={{ lat: selectedPosition.location.coordinates[0], lng: selectedPosition.location.coordinates[1] }}
         onCloseClick={handleInfoWindowClose}
       >
         <div>
-          <h2>New Marker</h2>
-          <p>Lat: {selectedPosition.lat}</p>
-          <p>Lng: {selectedPosition.lng}</p>
-        </div>
+
+    <RecipeReviewCard data={selectedPosition}/>
+    </div>
       </InfoWindowF>
     )}
   </GoogleMap>
